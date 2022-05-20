5af3eccb27034085d5a5a0d9e9e979074050cb08
cguill18 <clementineguillot23@gmail.com>
2022-05-20 16:07:38 +0200

    add planing + planning
    planning pas du tout complet (faire server)

---
+++ src/app/component/add_planning.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+
+
+const AddPlanning = ({loadPlannings}) => {
+    const [name,setName] = useState("");
+    const [date,setDate] = useState(new Date());
+    const [plannings,setPlannings] = useState([])
+
+    const validate = (e) => {
+        e.preventDefault();
+        let bodyLocal = JSON.stringify({name:name,date:date});
+        fetch('/api/planning',{
+            method:"POST",
+            body:bodyLocal,
+            headers: {
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            }
+        })
+            .then((res)=>{return res.json()})
+            .then((planningReponse)=>{
+                loadPlannings()
+            })
+    }
+
+    return (
+        <form onSubmit={validate}>
+            <label> name </label>
+            <input type="text" value={name} onChange={(e) => setName(e.currentTarget.value)}/>
+            <label> date </label>
+            <input type="date" value={date} onChange={(e) => setName(e.currentTarget.value)}/>
+            <button>+</button>
+        </form>
+    )
+}
+export default AddPlanning;
+
+
+
+
+
+
