4f57b50550592b7dda4cbd00250a85034a250c22
Louis Forestier <louis.forestier@etu.univ-poitiers.fr>
2022-05-24 21:35:43 +0200

    le planning est affiché directement sur la page d'accueil

--- src/app/visitor_app.jsx
+++ src/app/visitor_app.jsx
@@ -2,37 +2,22 @@ import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Routes, Route, Outlet} from "react-router-dom";
 import Main from "./component/main";
+import Signin from "./view/signin";
+import Planning from "./view/planning";
 
-const App = (props) => {
-    const [username,setUsername] = useState("");
-    const [password,setPassword] = useState("");
-
-    return (
-
-        <div>
-            <h1>Connection</h1>
-            <form method={'POST'} action={"/login"}>
-                <label htmlFor={"username"}>Username</label>
-                <input id={"username"} name={"username"} type={"text"} onChange={(e)=>setUsername(e.currentTarget.value)}/>
-                <label htmlFor={"password"}>Password</label>
-                <input id={"password"} name={"password"} type={"password"} onChange={(e)=>setPassword(e.currentTarget.value)}/>
-                <button type={"submit"}>Log in</button>
-            </form>
-        </div>
-    )
-}
 
 const root = ReactDOM.createRoot(
     document.getElementById("main")
 )
 
-const links =[{name:'Home',link:'/'},{name:'Login',link:'/'}];
+const links =[{name:'Plannings',link:'/'},{name:'Sign in',link:'/signin'}];
 
 root.render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Main links={links}/>}>
-                <Route index element={<App/>}></Route>
+                <Route index element={<Planning/>}/>
+                <Route path="/signin" element={<Signin/>}/>
             </Route>
         </Routes>
     </BrowserRouter>
