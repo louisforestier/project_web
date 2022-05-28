74196f102560f2d3383f9975a1d6f7b86d7d1af8
cguill18 <clementineguillot23@gmail.com>
2022-05-29 00:10:26 +0200

    ajout manche coté front

--- src/app/view/admin.jsx
+++ src/app/view/admin.jsx
@@ -6,13 +6,7 @@ class Admin extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            tokens: [],
-            token_id : "",
-            clients : [],
-            plannings : [],
-            manches : [],
-            planning_name: "",
-            planning_date: new Date()
+            tokens: []
         }
     }
 
