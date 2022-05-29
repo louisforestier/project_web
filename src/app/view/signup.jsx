f1fe4a0d8fa00ee800966affbdef4f2771a8867f
cguill18 <clementineguillot23@gmail.com>
2022-05-29 17:37:04 +0200

    sign up est plus joli

--- src/app/view/signup.jsx
+++ src/app/view/signup.jsx
@@ -36,18 +36,46 @@ class Signup extends React.Component {
             <div>
                 <h1>Sign Up</h1>
                 <form onSubmit={this.add}>
-                    <label> User name </label>
-                    <input type="text" value={this.state.username} onChange={(e) => this.setState({username:e.currentTarget.value})}/>
-                    <label> Password </label>
-                    <input type="password" value={this.state.password} onChange={(e) => this.setState({password:e.currentTarget.value})}/>
-                    <label> Confirm password </label>
-                    <input type="password" value={this.state.cpassword} onChange={(e) => this.setState({cpassword:e.currentTarget.value})}/>
-                    <br/>
-                    <label> First name </label>
-                    <input type="text" value={this.state.firstname} onChange={(e) => this.setState({firstname:e.currentTarget.value})}/>
-                    <label> Last name </label>
-                    <input type="text" value={this.state.lastname} onChange={(e) => this.setState({lastname:e.currentTarget.value})}/>
-                    <button>+</button>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td><label> User name </label></td>
+                            <td>
+                                <input type="text" value={this.state.username} onChange={(e) => this.setState({username:e.currentTarget.value})}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label> Password </label></td>
+                            <td>
+                                <input type="password" value={this.state.password} onChange={(e) => this.setState({password:e.currentTarget.value})}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label> Confirm password </label></td>
+                            <td>
+                                <input type="password" value={this.state.cpassword} onChange={(e) => this.setState({cpassword:e.currentTarget.value})}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label> First name </label></td>
+                            <td>
+                                <input type="text" value={this.state.firstname} onChange={(e) => this.setState({firstname:e.currentTarget.value})}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><label> Last name </label></td>
+                            <td>
+                                <input type="text" value={this.state.lastname} onChange={(e) => this.setState({lastname:e.currentTarget.value})}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>
+                                <button>Sign up</button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </form>
             </div>
         )
